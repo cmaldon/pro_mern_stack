d3997ec365046b69e3b96b183c3e78d0c44a6e4f
Celeste Maldonado <69225402+cmaldon@users.noreply.github.com>
2021-03-02 10:41:35 -0800

    Chapter 5
    Express and GraphQL

---
+++ src/App.jsx
@@ -0,0 +1,142 @@
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
+
+function jsonDateReviver(key, value) {
+  if (dateRegex.test(value)) return new Date(value);
+  return value;
+}
+
+class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the issue filter.</div>
+  );
+ }
+}
+
+function IssueRow(props) {
+    const issue = props.issue;
+    return (
+      <tr>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.due ? issue.due.toDateString() : ' '}</td>
+        <td>{issue.title}</td>
+      </tr>
+    );
+}
+
+function IssueTable(props) {
+    const issueRows = props.issues.map(issue => 
+      <IssueRow key={issue.id} issue={issue} />
+    );
+    
+    return (
+      <table className="bordered-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Created</th>
+            <th>Effort</th>
+            <th>Due Date</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+         {issueRows}
+        </tbody>
+      </table>
+  );
+}
+
+class IssueAdd extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.issueAdd;
+    const issue = {
+      owner: form.owner.value, title: form.title.value, 
+      due: new Date(new Date().getTime() + 1000*60*60*24*10),
+    }
+    this.props.createIssue(issue);
+    form.owner.value = ""; form.title.value = "";
+  }
+        
+  render() {
+    return ( 
+      <form name="issueAdd" onSubmit={this.handleSubmit}>
+        <input type="text" name="owner" placeholder="Owner" />
+        <input type="text" name="title" placeholder="Title" />
+        <button>Add</button>
+      </form>
+    );
+  }
+}
+
+class IssueList extends React.Component {
+  constructor() {
+    super();
+    this.state = { issues: [] };
+    this.createIssue = this.createIssue.bind(this);
+  }
+  
+  componentDidMount() {
+  this.loadData();
+  }
+  
+  async createIssue(issue) {
+    const query = `mutation {
+      issueAdd(issue:{
+        title: "${issue.title}",
+        owner: "${issue.owner}",
+        due: "${issue.due.toISOString()}",
+      }) {
+        id
+      }
+    }`;
+
+  const response = await fetch('/graphql', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query })
+  });
+  this.loadData();
+
+  const body = await response.text();
+  const result = JSON.parse(body, jsonDateReviver);
+  this.setState({ issues: result.data.issueList });
+  }
+  
+  createIssue(issue) {
+    issue.id = this.state.issues.length + 1;
+    issue.created = new Date();
+    const newIssueList = this.state.issues.slice();
+    newIssueList.push(issue);
+    this.setState({ issues: newIssueList });
+  }
+  
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable  issues={this.state.issues}/>
+        <hr />
+        <IssueAdd createIssue={this.createIssue} />
+      </React.Fragment>
+  );
+ }
+}
+
+const element = <IssueList />;
+
+ReactDOM.render(element, document.getElementById('contents'));
