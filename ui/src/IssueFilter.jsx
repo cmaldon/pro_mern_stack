5a40e45a45706e9e5504dba5a5954ffcf3665d5d
Celeste Maldonado <69225402+cmaldon@users.noreply.github.com>
2021-05-06 17:14:48 -0700

    Update IssueFilter.jsx

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -1,15 +1,20 @@
 /* eslint "react/prefer-stateless-function": "off" */
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 export default class IssueFilter extends React.Component {
   render() {
     return (
     <div>
-    <a href="/#/issues">All Issues</a>
-    {' | '}
-    <a href="/#/issues?status=New">New Issues</a>
-    {' | '}
-    <a href="/#/issues?status=Assigned">Assigned Issues</a>
+      <Link to="/issues">All Issues</Link>
+      {' | '}
+      <Link to={{ pathname: '/issues', search: '?status=New' }}>
+        New Issues
+      </Link>
+      {' | '}
+      <Link to={{ pathname: '/issues', search: '?status=Assigned' }}>
+       Assigned Issues
+      </Link>
     </div>
   );
  }
